b8e3ddac7fd8c1854b10d48c707e993be41cb1fe
vphamtyo <vpham.tyo@gmail.com>
2026-06-25 18:28:16 +0900

    Update functions.py jpn

--- whistleblowing_commons/functions.py
+++ whistleblowing_commons/functions.py
@@ -3,11 +3,11 @@ def seconds_to_minutes(seconds, return_string=True):
     reste = seconds % 60
     if return_string:
         if minutes == 0:
-            txt = f"{reste} seconds"
+            txt = f"{reste} 秒"
         else:
-            txt = f"{minutes} minutes"
+            txt = f"{minutes} 分"
             if reste:
-                txt += f" and {reste} seconds"
+                txt += f" {reste} 秒"
         return txt
     else:
         return minutes, reste
